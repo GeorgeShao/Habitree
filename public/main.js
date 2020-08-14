2b6378149071208d6dfaced9a119ed91aa958b64
Andrew Xue <andewzxue@gmail.com>
2020-08-14 18:17:26 -0400

    update database

--- public/main.js
+++ public/main.js
@@ -10,23 +10,19 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-firebase.database().ref('Andrew').set({
-    x : 3
-});
-
 function addValues(userID, type, value) {
-    firebase.database().ref(userID).once('value').then( snap => {
-        let currentValue = snap.val().type;
-        firebase.database().ref(userID).update({
-            type : value+currentValue
-        });
+    firebase.database().ref(userID+'/'+type).once('value').then( snap => {
+        let currentValue = snap.val();
+        console.log("current value "+currentValue);
+        var updates = {};
+        updates[type] = value+currentValue;
+        firebase.database().ref(userID).update(updates);
     });
 }
 
-addValues('Andrew', 'x', 4);
-
-firebase.database().ref('').on('value', snap => {
+firebase.database().ref('/').on('value', snap => {
     //update pictures
+    console.log("Something was updated");
 });
 
 
